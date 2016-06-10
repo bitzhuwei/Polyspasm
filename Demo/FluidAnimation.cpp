8dbb4ba6b4946896d4623559c85c8b88f0a21162
祝威 <bitzhuwei.cnblogs@qq.com>
2016-06-10 15:32:21 +0800

    test other letters

--- Demo/FluidAnimation.cpp
+++ Demo/FluidAnimation.cpp
@@ -61,7 +61,7 @@ FluidAnimation::FluidAnimation()
 	m_lines.push_back("not");
 	m_lines.push_back("ignite");
 	m_lines.push_back("your");
-	m_lines.push_back("computer");
+	m_lines.push_back("---___===+++Oo0");
 }
 
 void FluidAnimation::setup(Graphics& gfx)
